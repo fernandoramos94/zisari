c87269a2dbb40579cfb5fd5ce8bcf7af01d7ff6e
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-18 14:55:16 -0500

    cuarto commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,23 +3,31 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Zisare</title>
 
         <link rel="stylesheet" href="{!! asset('css/estilos.css') !!}" >
-        <link rel="stylesheet" href="{{ asset('css/main.css')}}" />
         
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
         <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="{{ asset('js/main.js') }}"></script>
+        <script src="http://zeptojs.com/zepto.min.js"></script>
+        <script type="text/javascript" src="{{asset('js/vegas.min.js')}}"></script>
 
     </head>
     <body>
         @yield('contenedor')
         <!-- <script src="{!! asset('js/bootstrap.min.js') !!}"></script> -->
         @yield('scripts')
+        <script type="text/javascript">
+            $("body").vegas({
+                slides: [
+                    { src: "/img/bg01.jpg" },
+                    { src: "/img/bg02.jpg" },
+                    { src: "/img/bg03.jpg" },
+                ]
+            });
+        </script>
     </body>
 </html>
