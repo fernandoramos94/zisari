ad8b45cb5d2b1077d5485889725a1bd813ff3f7f
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-18 14:41:34 -0500

    cuarto commit

--- public/js/main.js
+++ public/js/main.js
@@ -39,9 +39,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							'img/bg01.jpg': 'center',
+							'img/bg02.jpg': 'center',
+							'img/bg03.jpg': 'center'
 						},
 
 					// Delay.
