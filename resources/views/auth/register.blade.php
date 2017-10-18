7a68b6157601791559288d7e7f3ea4f2adfd48fa
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-18 11:32:36 -0500

    cuarto commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('contenedor')
-<!-- <div class="container">
+<div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -73,89 +73,5 @@
             </div>
         </div>
     </div>
-</div> -->
-
-<form method="POST" action="{{ route('register') }}" class="crearcuenta">
-    {{ csrf_field() }}
-    <div style="overflow: auto;">
-        <div id="list1">
-            <h1 class="text-center" style="color: #74accf"><B>Zisari</B></h1>
-            <h3 class="text">Crear Cuenta</h3>
-            <div class="form-group">
-                <label for="Nombre"><h5>Con las cuentas de Zisari se abre todo un mundo de ventajas para interactuar y conocer.</h5></label>
-                <input type="text" class="form-control" name="name" placeholder="Nombre de usuario" required>
-                <input type="text" class="form-control" name="email" placeholder="alguien@gmail.com" required>
-                <input type="password" class="form-control" name="password" id="password-confirm" placeholder="Crear contraseña" required>
-                <input type="password" class="form-control" id="Nombre" placeholder="Confirmar contraseña" name="password_confirmation" required>
-            </div>
-            <p class="text-center">
-                <a class="btn btn-primary btn-block" id="btn_link1">Siguiente</a>
-            </p>
-        </div>
-        <div id="list2">
-            <h1 class="text-center" style="color: #74accf"><B>Zisari</B></h1>
-            <h3 class="text">Crear Cuenta</h3>
-            <div class="form-group">
-                <label for="Nombre"><h5>Con las cuentas de Zisari se abre todo un mundo de ventajas para interactuar y conocer.</h5></label>
-                <input type="text" class="form-control" name="nombres" placeholder="Nombres" required>
-                <input type="text" class="form-control" name="apellidos" placeholder="Apellidos" required>
-                <select class="form-control" name="pais" style="margin: 15px 0;">
-                    <option>Seleccionar país</option>
-                </select>
-                <select class="form-control" name="genero" style="margin: 15px 0;">
-                    <option>Selecciona tu género</option>
-                    <option value="Hombre">Hombre</option>
-                    <option value="Mujer">Mujer</option>
-                </select>
-            </div>
-            <p class="text-center">
-                <a class="btn btn-primary btn-block" id="btn_link2">Siguiente</a>
-            </p>
-        </div>
-        <div id="list3">
-            <h1 class="text-center" style="color: #74accf"><B>Zisari</B></h1>
-            <h3 class="text">Crear Cuenta</h3>
-            <div class="form-group">
-                <label for="Nombre"><h5>Con las cuentas de Zisari se abre todo un mundo de ventajas para interactuar y conocer.</h5></label>
-            <div class="row">
-                <div class="col-md-4">
-                    <input type="text" class="form-control" name="dia" placeholder="Día" required >
-                </div>
-                <div class="col-md-4">
-                    <select class="form-control" name="mes">
-                        <option>Mes</option>
-                    </select>
-                </div>
-                <div class="col-md-4">
-                    <input type="text" class="form-control" name="anio" placeholder="Año" required>
-                </div>
-            </div>
-            <!-- <div class="input-group"></div>     -->
-                <input type="text" class="form-control" name="numeroCelular" placeholder="+(Código) Número de celular" required>
-                <input type="text" class="form-control" name="email" placeholder="Correo de Coinbase" required autofocus>
-                <input type="text" class="form-control" name="documento" placeholder="Documento de Identidad" required autofocus>
-            <!-- </div> -->
-            <p class="text-center">
-                <button type="submit" class="btn btn-primary btn-block">
-                    Register
-                </button>
-                <!-- <a class="btn btn-primary btn-block" id="btn_link3">Siguiente</a> -->
-            </p>
-        </div>
-
-    </div>
-    <!-- <div id="list4" style="display: none">
-        <h1 class="text-center" style="color: #74accf"><B>Zisari</B></h1>
-        <h3 class="text">Escribir contraseña</h3>
-        <div class="form-group">
-            <input type="text" class="form-control" name="" placeholder="Contraseña" required autofocus>
-        </div>
-        <p class="text-center">
-            <a class="btn btn-primary btn-block" id="btn_link4">Siguiente</a>
-        </p>
-    </div> -->
-
-</form>
-
-
+</div>
 @endsection
