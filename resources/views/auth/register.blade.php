06b97d2fc0fed8882474c92a214135675604b3ed
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-18 11:50:11 -0500

    cuarto commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,9 +5,6 @@
         {{ csrf_field() }}
 
         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-            <label for="name" class="col-md-4 control-label">Name</label>
-
-            <div class="col-md-6">
                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
                 @if ($errors->has('name'))
@@ -15,7 +12,6 @@
                         <strong>{{ $errors->first('name') }}</strong>
                     </span>
                 @endif
-            </div>
         </div>
 
         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
