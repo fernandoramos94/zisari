276091653323af8ad649bf91d52e6470bebdbe9a
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-18 13:41:43 -0500

    cuarto commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,16 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-
+                <a href="{{ url('/logout') }}" onclick="event.preventDefault();
+                 document.getElementById('logout-form').submit();">
+                  Logout
+                </a>
+             <form id="logout-form" 
+                    action="{{ url('/logout') }}" 
+                method="POST" 
+                style="display: none;">
+                            {{ csrf_field() }}
+              </form>
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
