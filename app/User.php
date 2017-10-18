a3e945f3e8b1baacfacce6512579308b4a886616
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-18 12:49:20 -0500

    cuarto commit

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'pais', 'pais','genero','nombres','apellidos','dia','mes','anio','numeroCelular','documento','correo','portado','imagen'
     ];
 
     /**
