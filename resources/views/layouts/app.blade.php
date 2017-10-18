aecc209799f6b77598793cb67820f165b271c2fc
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-18 13:58:54 -0500

    cuarto commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,36 +8,16 @@
         <title>Zisare</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
-        <script src="{{ asset('js/jquery-3.1.1.min.js') }}"></script>
-        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-        <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
-        <title>Zisare</title>
+        <link rel="stylesheet" href="{!! asset('css/estilos.css') !!}">
+        <script src="{!! asset('js/jquery-3.1.1.min.js') !!}"></script>
+        <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
 
         <!--{!! asset('js/app.min.js') !!}-->
 
-        <!-- Styles -->
         <link />
     </head>
     <body>
         @yield('contenedor')
-        
-        <!-- <script type="text/javascript">
-            $(document).ready(function(){
-                $("#btn_link1").on("click", function(){
-                    $("#list1").hide();
-                    $("#list2").show();
-                });
-                $("#btn_link2").on("click", function(){
-                    $("#list2").hide();
-                    $("#list3").show();
-                });
-                $("#btn_link3").on("click", function(){
-                    $("#list3").hide();
-                    $("#list4").show();
-                });
-            });
-        </script> -->
         @yield('scripts')
     </body>
 </html>
