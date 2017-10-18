0adcbda7eefba1b59d58124d9172728dc40e5360
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-18 14:14:47 -0500

    cuarto commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -157,8 +157,8 @@
             </div>
             <div class="form-group">
                 <div class="col-md-6 col-md-offset-4">
-                    <button type="submit" class="btn btn-primary">
-                        Register
+                    <button type="submit" class="btn btn-primary btn-block">
+                        Registrame
                     </button>
                 </div>
             </div>
