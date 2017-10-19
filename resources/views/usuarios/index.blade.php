bdb59643e32ac22d7c47b70212f4539cad53f57e
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-19 13:42:58 -0500

    m

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -1,29 +1,35 @@
 @extends('layouts.layoutBackend')
 @section('contenedorBackend')
 <div class="col-lg-12">
-	<table class="table table-bordered">
-		<thead>
-			<tr>
-				<th>Nombre</th>
-				<th>Apellidos</th>
-				<th>País</th>
-				<th>Ip</th>
-				<th>Ver Perfil</th>
-				<th>Opción</th>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach ($usuario as $user)
-				<tr>
-					<td>{{$user->nombres}}</td>
-					<td>{{$user->apellidos}}</td>
-					<td>{{$user->pais}}</td>
-					<td>45</td>
-					<td><a>Ver </a></td>
-					<td><a > Bloquear Usuario </a></td>
-				</tr>
-			@endforeach
-		</tbody>
-	</table>
+	<div class="panel panel-default">
+  		<div class="panel-heading">Base de Datos de Registro de Usuarios</div>
+  		<div class="panel-body">
+  			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>Nombre</th>
+						<th>Apellidos</th>
+						<th>País</th>
+						<th>Ip</th>
+						<th>Ver Perfil</th>
+						<th>Opción</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach ($usuario as $user)
+						<tr>
+							<td>{{$user->nombres}}</td>
+							<td>{{$user->apellidos}}</td>
+							<td>{{$user->pais}}</td>
+							<td>45</td>
+							<td><a>Ver </a></td>
+							<td><a > Bloquear Usuario </a></td>
+						</tr>
+					@endforeach
+				</tbody>
+			</table>
+  		</div>
+	</div>
+	
 </div>
 @stop
