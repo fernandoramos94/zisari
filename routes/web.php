89d0755a1c6eb47b4b38a1e7ab1d3f95182699fa
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-19 09:30:21 -0500

    se agrega controladores

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('auth.login');
 });
+Route::get('/usuarios', 'UsuariosController@index')->name('usuarios');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
