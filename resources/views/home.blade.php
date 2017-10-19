add903bd0bc0ab948b668ac6490d5e4bb8587cdb
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-19 12:45:59 -0500

    m

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,43 @@
 @extends('layouts.layoutBackend')
 
-@section('contenedor')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-                <a href="{{ url('/logout') }}" onclick="event.preventDefault();
-                 document.getElementById('logout-form').submit();">
-                  Logout
-                </a>
-             <form id="logout-form" 
-                    action="{{ url('/logout') }}" 
-                method="POST" 
-                style="display: none;">
-                            {{ csrf_field() }}
-              </form>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+@section('contenedorBackend')
+<div class="col-md-3 col-sm-6">
+    <div class="dashboard-tile detail tile-red">
+        <div class="content">
+            <h1 class="text-left timer" data-from="0" data-to="180" data-speed="2500"> </h1>
+            <p>Total de Usuarios</p>
+        </div>
+        <div class="icon"><i class="fa fa-users"></i>
+        </div>
+    </div>
+</div>
+<div class="col-md-3 col-sm-6">
+    <div class="dashboard-tile detail tile-turquoise">
+        <div class="content">
+            <h1 class="text-left timer" data-from="0" data-to="56" data-speed="2500"> </h1>
+            <p>Total de Pagos</p>
+        </div>
+        <div class="icon"><i class="fa fa-users"></i>
+        </div>
+    </div>
+</div>
+<div class="col-md-3 col-sm-6">
+    <div class="dashboard-tile detail tile-blue">
+        <div class="content">
+            <h1 class="text-left timer" data-from="0" data-to="32" data-speed="2500"> </h1>
+            <p>Usuarios Bloqueados</p>
+        </div>
+        <div class="icon"><i class="fa fa fa-users"></i>
+        </div>
+    </div>
+</div>
+<div class="col-md-3 col-sm-6">
+    <div class="dashboard-tile detail tile-purple">
+        <div class="content">
+            <h1 class="text-left timer" data-to="105" data-speed="2500"> </h1>
+            <p>Usuarios Pagados</p>
+        </div>
+        <div class="icon"><i class="fa fa-users"></i>
         </div>
     </div>
 </div>
--- resources/views/layouts/layoutBackend.blade.php
+++ resources/views/layouts/layoutBackend.blade.php
@@ -192,46 +192,7 @@
             <section id="main-content">
                 <!--tiles start-->
                 <div class="row">
-                    <div class="col-md-3 col-sm-6">
-                        <div class="dashboard-tile detail tile-red">
-                            <div class="content">
-                                <h1 class="text-left timer" data-from="0" data-to="180" data-speed="2500"> </h1>
-                                <p>Total de Usuarios</p>
-                            </div>
-                            <div class="icon"><i class="fa fa-users"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3 col-sm-6">
-                        <div class="dashboard-tile detail tile-turquoise">
-                            <div class="content">
-                                <h1 class="text-left timer" data-from="0" data-to="56" data-speed="2500"> </h1>
-                                <p>Total de Pagos</p>
-                            </div>
-                            <div class="icon"><i class="fa fa-users"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3 col-sm-6">
-                        <div class="dashboard-tile detail tile-blue">
-                            <div class="content">
-                                <h1 class="text-left timer" data-from="0" data-to="32" data-speed="2500"> </h1>
-                                <p>Usuarios Bloqueados</p>
-                            </div>
-                            <div class="icon"><i class="fa fa fa-users"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3 col-sm-6">
-                        <div class="dashboard-tile detail tile-purple">
-                            <div class="content">
-                                <h1 class="text-left timer" data-to="105" data-speed="2500"> </h1>
-                                <p>Usuarios Pagados</p>
-                            </div>
-                            <div class="icon"><i class="fa fa-users"></i>
-                            </div>
-                        </div>
-                    </div>
+                    @yield('contenedorBackend')
                 </div>
             </section>
         </section>
