05a78cfb8f37621e74f5068e3d08d3666e75a44e
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-20 10:40:58 -0500

    cambios

--- resources/views/logo/nuevo.blade.php
+++ resources/views/logo/nuevo.blade.php
@@ -57,24 +57,44 @@
 			<div class="panel panel-default">
   				<div class="panel-heading">Configurar y Selecionar Imagen como Titulo</div>
   				<div class="panel-body">
-  					<label>Subir Imagen</label>
-  					<input type="file" name="">
-  					<br>
-  					<label>Enlace de Imagen</label>
-  					<br>
-  					<input type="text" name="" class="form-control">
-  					<br>
-  					<label>Definir Fecha de Inicio y de Finalizacion</label>
-  					<br>
-  					<label>
-  						Inicio
-  					</label>
-  					<input type="date" name="" class="form-control">
-  					<input type="text" name="" class="form-control">
-  					<br>
-  					<label>Finalización</label>
-  					<input type="date" name="" class="form-control">
-  					<input type="text" name="" class="form-control">
+            <div class="form-group">
+  					  <label>Subir Imagen</label>
+  					   <input type="file" name="">
+            </div>
+            <div class="form-group">
+  					   <label>Enlace de Imagen</label>
+  					   <input type="text" name="" class="form-control">
+            </div>
+            <div class="form-control">
+  					   <label>Definir Fecha de Inicio y de Finalizacion</label>
+            </div>
+            <div class="form-group">
+              <div class="row">
+                <div class="col-lg-3">
+    					     <label>Inicio</label>
+                </div>
+                <div class="col-lg-6">
+    					   <input type="date" name="" class="form-control">
+                  
+                </div>
+                <div class="col-lg-3">
+    					     <input type="text" name="" class="form-control">
+                </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="row">
+                <div class="col-lg-4">
+  					       <label>Finalización</label>
+                </div>
+                <div class="col-lg-4">
+  					       <input type="date" name="" class="form-control">
+                </div>
+                <div class="col-lg-4">
+  					       <input type="text" name="" class="form-control">
+                </div>
+              </div>
+            </div>
   					<br>
   					<a href="" class="btn btn-success">Guardar</a>
   				</div>
