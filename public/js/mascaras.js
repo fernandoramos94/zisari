d1082a52e03e05d7726f317ac4ea85745dd5a6d7
unknown <ferrati94100112668@gmail.com>
2017-10-20 23:08:11 -0500

    se subio cambios

--- public/js/mascaras.js
+++ public/js/mascaras.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 	// formulario de registro
 	$("#dia").mask("00", {placeholder: "Día"});
-	$("#documento").mask("000000000000000", {placeholder: "Día"});
-	$("#anio").mask("0000", {placeholder: "Día"});
+	$("#documento").mask("000000000000000", {placeholder: "Documento"});
+	$("#anio").mask("0000", {placeholder: "Año"});
 	$('#numeroCelular').mask('+(00) 000 000 00 00', {placeholder: "+(Código) Número de Móbil"});
 
 
