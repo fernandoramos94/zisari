f5511427b84c1abe160cfd4af1736a21c9a54323
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-23 14:42:47 -0500

    layout usuario

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.layoutUsuario')
 @section('contenedorLayout')
 	<div class="col-lg-8">
-		<ul class="nav nav-tabs" role="tablist">
-			<li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Zisaricoins Gratis</a></li>
-			<li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Multiplicador Zisaricoins</a></li>
-			<li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Puntos</a></li>
-			<li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Preguntas Frecuentes</a></li>
+		<ul class="nav nav-tabs" role="tablist" style="background: #f6f7f9">
+			<li role="presentation" class="active"><a style="color: #000" href="#home" aria-controls="home" role="tab" data-toggle="tab">Zisaricoins Gratis</a></li>
+			<li role="presentation"><a style="color: #000" href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Multiplicador Zisaricoins</a></li>
+			<li role="presentation"><a style="color: #000" href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Puntos</a></li>
+			<li role="presentation"><a style="color: #000" href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Preguntas Frecuentes</a></li>
 		</ul>
 
 		<!-- Tab panes -->
