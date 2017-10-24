bd9b519389ab52d508ffa7b567a701611b8c49c2
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-24 12:48:37 -0500

    admin user

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -23,12 +23,14 @@
 		</div>
 	</div>
 	<div class="col-lg-4">
-		<div class="col-lg-8 anuncio">
-			<p><span class="textPublicidad">Publicidad</span><a class="linkCrearAnuncio" href="">Crear Anuncio</a></p>
-			<img src="http://img.aws.ehowcdn.com/intl-620/ds-cdn-write/upload//8000/900/50/3/288953.jpg">
-			<h4>¿Por qué Uruguay perdió el repechaje?</h4>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua.
+		<div class="row">
+			<div class="col-lg-8 anuncio">
+				<p><span class="textPublicidad">Publicidad</span><a class="linkCrearAnuncio" href="">Crear Anuncio</a></p>
+				<img src="http://img.aws.ehowcdn.com/intl-620/ds-cdn-write/upload//8000/900/50/3/288953.jpg">
+				<h4>¿Por qué Uruguay perdió el repechaje?</h4>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.
+			</div>
 		</div>
 	</div>
 @stop()
