e0e3bfee97c284ecb13340cf730ea6d049b2b2b1
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-25 09:54:23 -0500

    cambios

--- resources/views/layouts/layoutUsuario.blade.php
+++ resources/views/layouts/layoutUsuario.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Administrador - Zisari</title>
     <meta name="description" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <!-- Favicon -->
     <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
