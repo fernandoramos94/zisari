a1168d2640291de7ec70478aad2141dcbb7822f9
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-25 17:27:52 -0500

    cambios

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -91,12 +91,12 @@
                             <div class="panel panel-default">
                                 <div class="panel-heading">
                                     <h4 class="panel-title">
-                              			<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" class="collapsed">
+                              			<a data-toggle="collapse" data-parent="#accordion" href="#collapseMulOne" class="collapsed">
                                 			¿Cómo funciona?
                               			</a>
                             		</h4>
                                 </div>
-                                <div id="collapseOne" class="panel-collapse collapse">
+                                <div id="collapseMulOne" class="panel-collapse collapse">
                                 	<div class="panel-body">
                                         Usted hace una apuesta y elije Hi o Lo, y si sale la opción seleccionada usted gana, en caso de salir lo contrario pierde lo que apostó. 
                                     </div>
@@ -105,12 +105,12 @@
                             <div class="panel panel-default">
                                 <div class="panel-heading">
                                     <h4 class="panel-title">
-                              			<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                              			<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseMulTwo">
                                 			¿Cuánto puedo ganar en el Multiplicador?
                               			</a>
                         			</h4>
                                 </div>
-                            	<div id="collapseTwo" class="panel-collapse collapse">
+                            	<div id="collapseMulTwo" class="panel-collapse collapse">
                                     <div class="panel-body">
                                         La cantidad que puedes ganar depende del total de la apuesta y el número de veces que jueges.
                                     </div>
