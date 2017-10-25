e3ddfd378b9563f5e171a031c5f394cb4d87e61f
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-25 16:13:43 -0500

    cambios

--- resources/views/layouts/layoutUsuario.blade.php
+++ resources/views/layouts/layoutUsuario.blade.php
@@ -501,7 +501,7 @@
                             source: array,
                             highlighter: function(item) {
                                 var parts = item.split('#'),
-                                html = '<div>';
+                                html = '<div class="col-lg-12">';
                                 html += '<div class="profile-photo">';
                                 html += '<img src="assets/img/avatar2.gif" alt="" class="img-circle">';
                                 html += '</div>';
