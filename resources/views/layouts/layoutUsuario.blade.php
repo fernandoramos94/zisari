fbd48fda16f081ae2c1c84528fa0ea6695b2df7a
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-25 16:17:30 -0500

    cambios

--- resources/views/layouts/layoutUsuario.blade.php
+++ resources/views/layouts/layoutUsuario.blade.php
@@ -529,7 +529,7 @@
     </script>
     <style type="text/css">
         ul.typeahead.dropdown-menu{
-            width: 33%;
+            width: 350px;
         }
         ul.typeahead.dropdown-menu > li.active > a{
             background: #1bbc9b;
