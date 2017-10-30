3dff6572e09c9302929e14cc772359e3286f950a
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-30 10:05:39 -0500

    condicional en h1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
      ?>
     <form class="crearcuenta" method="POST" action="{{ route('login') }}">
         {{ csrf_field() }}
-        <input type="hidden" id="urlRoute" value="{{URL:to('')}}">
+        <input type="hidden" id="urlRoute" value="{{URL::to('')}}">
         @foreach ($text as $title)
             @if ($title->endDate > $fechaActual && $title->longTitle != "" || $title->longTitle != null)
                 <h1 class="text-center" style="color: #74accf"><B>{{$title->longTitle}}</B></h1>
