9254ce59b3b366c7405e014bb357b676c1bd9112
Fernando Ramos <Fernando.Ramos@glogic.com.co>
2017-10-30 14:21:03 -0500

    condicional en h1

--- project/app/Http/Controllers/LogoImgController.php
+++ project/app/Http/Controllers/LogoImgController.php
@@ -23,7 +23,7 @@ class LogoImgController extends Controller
         if ($file = $request->file('file')) {
             $file     = $request->file('file');
             $nameComp = md5(time()) . '.' . $file->getClientOriginalExtension();
-            $path     = '/img/logo/';
+            $path     = './img/logo/';
             $file->move($path, $nameComp);
         }
         else{
@@ -32,7 +32,7 @@ class LogoImgController extends Controller
         if ($logoMini = $request->file('fileMini')) {
             $logoMini     = $request->file('fileMini');
             $nameCompMini = md5(time()) . '.' . $logoMini->getClientOriginalExtension();
-            $path     = '/img/logo/';
+            $path = './img/logo/';
             $logoMini->move($path, $nameCompMini);
         }
         else{
